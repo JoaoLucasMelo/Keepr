c1202ad5eed0171c8a90a25f2b28f8aee8175a35
John Melo <joaolucasrmelo@gmail.com>
2022-01-07 11:22:02 -0700

    tables

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -6,3 +6,40 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+
+CREATE TABLE IF NOT EXISTS keeps(
+  id INT NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name TEXT NOT NULL COMMENT 'Keep Name',
+  description TEXT NOT NULL COMMENT 'Keep Description',
+  imgUrl TEXT NOT NULL,
+  views INT NOT NULL,
+  shares INT NOT NULL,
+  keeps INT NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS vaults(
+  id INT NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name TEXT NOT NULL COMMENT 'Vault Name',
+  description TEXT NOT NULL COMMENT 'Vault Description',
+  imgUrl TEXT NOT NULL,
+  isPrivate TINYINT NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS keeps(
+  id INT NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  creatorId VARCHAR(255) NOT NULL,
+  vaultId INT NOT NULL,
+  keepId INT NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+  FOREIGN KEY (vaultId) REFERENCES vaults(id) ON DELETE CASCADE,
+  FOREIGN KEY (keepId) REFERENCES keeps(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
