e8fc87cdff7189456087735a50aa889da788f87c
John Melo <joaolucasrmelo@gmail.com>
2022-01-14 10:22:53 -0700

    Delete bin directory

--- bin/Release/net5.0/publish/wwwroot/assets/AboutPage.7cc97b3b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.7a412f31.js";import{o,b as t,d as s}from"./vendor.7c4d75cf.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,f,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
